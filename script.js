26e9a061d37707caaaa1fc310337aca306f4a52d
Riviane Rose <rivianerose@users.noreply.github.com>
2022-10-10 14:53:14 -0300

    fixed

--- script.js
+++ script.js
@@ -2,10 +2,22 @@
 // clicar = desaparecer o menu 
 // criar um inner html com lis opcao 2 
 
-function clicar() {
-    let hamburguer = document.getElementById('imagemjs').innerHTML = '<img src="./imagens/cardapio.png" alt=""> <br> <li> Home</li> <br> <li> Shop</li>  <br> <li> Contact</li>'
-}
+//function clicar() {
 
-function desaparecer(){
-let sair = document.getElementById('imagemjs').innerHTML = '<img src="./imagens/cardapio.png" alt=""> '
+    //let hamburguer = document.getElementById('imagemjs').innerHTML = '<img src="./imagens/cardapio.png" alt=""> <br> <li> Home</li> <br> <li> Shop</li>  <br> <li> Contact</li>'
+//}
+
+//function desaparecer(){
+//let sair = document.getElementById('imagemjs').innerHTML = '<img src="./imagens/cardapio.png" alt=""> '
+//}
+
+function clicar() {
+    let abrir = document.getElementById('menuresponsivo')
+if (abrir.classList.contains('show')){
+    abrir.classList.remove('show')
+    abrir.classList.add('hide')
+}else {
+    abrir.classList.remove('hide')
+    abrir.classList.add('show')
+}
 }
